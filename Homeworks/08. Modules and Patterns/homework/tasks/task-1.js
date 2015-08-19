4f8f9a7c50ccceea756a3313a9ddc96d363c205d
iKostov86 <i02071986>
2015-08-19 22:42:14 +0300

    homework is done

--- Homeworks/08. Modules and Patterns/homework/tasks/task-1.js
+++ Homeworks/08. Modules and Patterns/homework/tasks/task-1.js
@@ -45,23 +45,187 @@
 */
 
 function solve() {
-	var Course = {
-		init: function(title, presentations) {
-		},
-		addStudent: function(name) {
-		},
-		getAllStudents: function() {
-		},
-		submitHomework: function(studentID, homeworkID) {
-		},
-		pushExamResults: function(results) {
-		},
-		getTopStudents: function() {
-		}
-	};
-
-	return Course;
-}
-
+    var Course = (function () {
+        var studentsID = 0,
+            students = [];
+    
+        var Course = {
+            init: function (title, presentations) {
+                this.title = title;
+                this.presentations = presentations;
+                return this;
+            },
+            addStudent: function (name) {
+                if (!validateStudent(name)) {
+                    throw Error;
+                } return studentsID;
+            },
+            getAllStudents: function () {
+                return students;
+            },
+            submitHomework: function (studentID, homeworkID) {
+                if (!validateID(studentID) || !validateID(homeworkID) ||
+                    studentID > studentsID || homeworkID > this.presentations.length) {
+                    throw Error;
+                }
+            },
+            pushExamResults: function (results) {
+                var index,
+                    j;
+                
+                if (Array.isArray(results)) {
+                    var arr = [];
+                    for (index = 0; index < results.length; index += 1) {
+                        arr.push(results[index].StudentID);
+                        if (!validateID(results[index].StudentID) ||
+                            results[index].StudentID > studentsID ||
+                            !validateNumber(results[index].score)) {
+                            throw Error;
+                        }
+                    }
+                    if (!chekForDuplicates(arr)) {
+                        throw Error;
+                    }
+                } else {
+                    throw Error;
+                }
+                
+                for (index = 0; index < students.length; index += 1) {
+                    for (j = 0; j < results.length; j += 1) {
+                        if (students[index].id == results[j].StudentID) {
+                            students[index].score = results[j].score;
+                        }
+                    }
+                    if (!students[index].score) {
+                        students[index].score = 0;
+                    }
+                }
+            },
+            getTopStudents: function() {
+            }
+        };
+        
+        Object.defineProperties(Course, {
+            'title': {
+                get: function () {
+                    return this._title;
+                },
+                set: function (value) {
+                    if (!validateTitle(value)) {
+                        throw Error;
+                    }
+                    this._title = value;
+                }
+            },
+            'presentations': {
+                get: function () {
+                    return this._presentations;
+                },
+                set: function (value) {
+                    if (!validatePresentations(value)) {
+                        throw Error;
+                    }
+                    this._presentations = value;
+                }
+            }
+        });
+        
+        function validateTitle (title) {
+            var index,
+                length = title.length;
+                
+            if (title && title[0] !== ' ' && title[length - 1] !== ' ') {
+                for (index = 1; index < length - 2; index += 1) {
+                    if (title[index] === ' ' && title[index + 1] === ' ') {
+                        return false;
+                    }
+                } return true;
+            } return false;
+        }
+        
+        function validatePresentations (presentations) {
+            var index,
+                length = presentations.length;
+                
+            if (presentations && length !== 0) {
+                for (index = 0; index < length; index += 1) {
+                    if (!validateTitle(presentations[index])) {
+                        return false;
+                    }
+                } return true;
+            } return false;
+        }
+        
+        function validateStudent(student) {
+            var names;
+            
+            if (validateString(student)) {
+                names = student.split(' ');
+                if (names.length === 2) {
+                    if (validateName(names[0]) && validateName(names[1])) {
+                        students.push({
+                            firstname: names[0],
+                            lastname: names[1],
+                            id: ++studentsID
+                        });
+                        return true;
+                    }
+                }
+            } return false;
+        }
+        
+        function validateName (name) {
+            var index,
+                length;
+            if (name[0] !== name[0].toUpperCase()) {
+                return false;
+            }
+            for (index = 1, length = name.length; index < length; index += 1) {
+                if (name[index] !== name[index].toLowerCase()) {
+                    return false;
+                }
+            } return true;
+        }
+        
+        function validateString(str) {
+            if (typeof(str) === 'string') {
+                return true;
+            } return false;
+        }
+        
+        function validateNumber(number) {
+            if (isNaN(number) || typeof(number) !== 'number') {
+                return false;
+            } return true;
+        }
+        
+        function validateID(identificator) {
+            var parsed = parseInt(identificator);
+            
+            if (isNaN(parsed) || parsed !== identificator || parsed < 1) {
+                return false;
+            } return true;
+        }
+        
+        function chekForDuplicates(arr) {
+            var i,
+                len=arr.length,
+                out=[],
+                obj={};
+            
+            for (i=0;i<len;i++) {
+                obj[arr[i]]=0;
+            }
+            for (i in obj) {
+                out.push(i);
+            }
+            return out.length === arr.length;
+        }
+    
+        return Course;
+    })();
+    
+    return Course;
+};
 
 module.exports = solve;
